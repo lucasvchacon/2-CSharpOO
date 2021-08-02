9f57f7e5f304349bd753eff9ba03abe2f5f31a33
lucas <lucasvchacon1020@gmail.com>
2021-08-02 13:07:50 -0300

    introOO

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace introducaoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ContaCorrente ();
+
+            ContaCorrente contaDoLucas = new ContaCorrente();
+
+            contaDoLucas.titular = " Lucas";
+            Console.WriteLine(contaDoLucas.saldo);
+            contaDoLucas.Sacar (50);
+            Console.WriteLine(contaDoLucas.saldo);
+
+            contaDoLucas.agencia = 1883;
+            contaDoLucas.numero = 1920;
+            contaDoLucas.saldo = 100;
+
+            contaDoLucas.saldo += 200;
+
+            Console.WriteLine(contaDoLucas.titular);
+            Console.WriteLine("Agência: " + contaDoLucas.agencia);
+            Console.WriteLine("Número: " + contaDoLucas.numero);
+            Console.WriteLine("Saldo: " + contaDoLucas.saldo);
+
+            
+
+           
+            
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
